c18ca93f0a0d65f231d249cdd0283aaa58b33058
Louis <louisreynard919@gmail.com>
2022-05-31 17:48:30 +0200

    :bug: ui done

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:app/music.dart';
+import 'package:app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'page_manager.dart';
+import 'package:app/utils/utils.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +10,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Soundify App',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Soundify'),
     );
@@ -48,121 +47,83 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  List<Music> myMusicList = [
-    Music(
-      'Haine & Sex',
-      'Gazo',
-      'assets/gazo.jpg',
-      'assets/gazo.mp3',
-    ),
-    Music(
-      'Alicante',
-      'Gambino',
-      'assets/gambino.jpg',
-      'assets/gambino.mp3',
-    ),
-    Music(
-      'Time',
-      'Josman',
-      'assets/josman.jpg',
-      'assets/josman.mp3',
-    )
-  ];
-
   int position = 0;
   int get listSize => myMusicList.length;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //center the appbar
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      //print the data of the first music in a square
+      backgroundColor: const Color.fromARGB(204, 60, 60, 60),
+      appBar: appBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              '${myMusicList[position].title}',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            Text(
-              '${myMusicList[position].singer}',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            Image.asset(
-              '${myMusicList[position].imagePath}',
-              width: 200,
-              height: 200,
-            ),
-            const Spacer(),
-            ValueListenableBuilder<ProgressBarState>(
-              valueListenable: _pageManager.progressNotifier,
-              builder: (_, value, __) {
-                return ProgressBar(
-                  progress: value.current,
-                  buffered: value.buffered,
-                  total: value.total,
-                  onSeek: _pageManager.seek,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      //precedent, play and next button
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.skip_previous),
-              onPressed: () {
-                setState(() {
-                  position = (position - 1) % listSize;
-                  _pageManager.changeSong(myMusicList[position].urlSong);
-                });
-              },
-            ),
-            ValueListenableBuilder<ButtonState>(
-              valueListenable: _pageManager.buttonNotifier,
-              builder: (_, value, __) {
-                switch (value) {
-                  case ButtonState.loading:
-                    return Container(
-                      margin: const EdgeInsets.all(8.0),
-                      width: 32.0,
-                      height: 32.0,
-                      child: const CircularProgressIndicator(),
-                    );
-                  case ButtonState.paused:
-                    return IconButton(
-                      icon: const Icon(Icons.play_arrow),
-                      iconSize: 32.0,
-                      onPressed: _pageManager.play,
-                    );
-                  case ButtonState.playing:
-                    return IconButton(
-                      icon: const Icon(Icons.pause),
-                      iconSize: 32.0,
-                      onPressed: _pageManager.pause,
-                    );
-                }
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.skip_next),
-              onPressed: () {
-                setState(() {
-                  position = (position + 1) % listSize;
-                  _pageManager.changeSong(myMusicList[position].urlSong);
-                });
-              },
-            ),
+            space(40),
+            songPicture(myMusicList[position].imagePath),
+            space(50),
+            progressBar(_pageManager),
+            space(50),
+            songTitle(myMusicList[position].title),
+            space(10),
+            songArtist(myMusicList[position].singer),
+            space(70),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.skip_previous),
+                  iconSize: 40,
+                  color: Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      position = (position - 1) % listSize;
+                      _pageManager.changeSong(myMusicList[position].urlSong);
+                    });
+                  },
+                ),
+                ValueListenableBuilder<ButtonState>(
+                  valueListenable: _pageManager.buttonNotifier,
+                  builder: (_, value, __) {
+                    switch (value) {
+                      case ButtonState.loading:
+                        return Container(
+                          margin: const EdgeInsets.all(8.0),
+                          width: 40.0,
+                          height: 40.0,
+                          color: Colors.white,
+                          child: const CircularProgressIndicator(),
+                        );
+                      case ButtonState.paused:
+                        return IconButton(
+                          icon: const Icon(Icons.play_arrow),
+                          color: Colors.white,
+                          iconSize: 40.0,
+                          onPressed: _pageManager.play,
+                        );
+                      case ButtonState.playing:
+                        return IconButton(
+                          icon: const Icon(Icons.pause),
+                          color: Colors.white,
+                          iconSize: 40.0,
+                          onPressed: _pageManager.pause,
+                        );
+                    }
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.skip_next),
+                  iconSize: 40,
+                  color: Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      position = (position + 1) % listSize;
+                      _pageManager.changeSong(myMusicList[position].urlSong);
+                    });
+                  },
+                ),
+              ],
+            )
           ],
         ),
       ),
---
+++ app/lib/utils/utils.dart
@@ -0,0 +1,34 @@
+import 'package:app/music.dart';
+
+List<Music> myMusicList = [
+  Music(
+    'Fixette',
+    'Ziak',
+    'assets/pictures/ziak.jpg',
+    'assets/songs/ziak.mp3',
+  ),
+  Music(
+    'Haine & Sex',
+    'Gazo',
+    'assets/pictures/gazo.jpg',
+    'assets/songs/gazo.mp3',
+  ),
+  Music(
+    'Alicante',
+    'Gambino',
+    'assets/pictures/gambino.jpg',
+    'assets/songs/gambino.mp3',
+  ),
+  Music(
+    'Time',
+    'Josman',
+    'assets/pictures/josman.jpg',
+    'assets/songs/josman.mp3',
+  ),
+  Music(
+    'Red Bull',
+    'UZI',
+    'assets/pictures/uzi.jpg',
+    'assets/songs/uzi.mp3',
+  )
+];
